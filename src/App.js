591491571d43d97cf47bb26431ebb26943099f36
LaySent <laysent@gmail.com>
2018-10-10 20:43:45 +0800

    modify demo to illustrate the issue better

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      status: '',
+    };
     this.onChange = (editorState) => this.setState({ editorState });
   }
   render() {
     return (
-      <Editor
-        editorState={this.state.editorState}
-        onChange={this.onChange}
-        handleReturn={() => { console.log('Enter pressed.'); return 'handled'; }}
-        handleDroppedFiles={() => { console.log('Dropped.'); return 'handled'; }}
-      />
+      <div>
+        <h2>How to reproduce the issue:</h2>
+        <ol>
+          <li>{"Press enter should be handled by handleReturn (handleReturn={()=>'handled'}"}</li>
+          <li>Drag a file and drop it</li>
+          <li>Drag a file and not drop it</li>
+          <li>Press enter will not be handled anymore (bug)</li>
+        </ol>
+        <div>Current Status of callback: {this.state.status}</div>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          handleReturn={() => { this.setState({ status: 'handleReturn is triggered.' }); return 'handled'; }}
+          handleDroppedFiles={() => { this.setState({ status: 'hanldeDroppedFiles is triggered.' }); return 'handled'; }}
+        />
+      </div>
     );
   }
 }
